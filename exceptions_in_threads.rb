ed1eb36eec7a0551c70c66591c2e9592f9b4695d
Serge Kislak <S.Kisliak@sam-solutions.net>
2013-09-09 18:46:46 +0300

    Updated exception example.

--- exceptions_in_threads.rb
+++ exceptions_in_threads.rb
@@ -1,4 +1,7 @@
+puts 'Thread.abort_on_exception(default) = ' + Thread.abort_on_exception.to_s
+
 Thread.abort_on_exception = rand > 0.5 # Can be set to true or false
+puts 'Thread.abort_on_exception = ' + Thread.abort_on_exception.to_s
 
 begin
   t = Thread.new do
@@ -7,7 +10,7 @@ begin
   end
 
   puts 1
-  sleep 1
+  sleep 5
   puts 2
 
   t.join
